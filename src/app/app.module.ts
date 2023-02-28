9e8f35d22d366b218a03f9325902dfad261e1713
Leticia Junqueira <leticia_junqueira10@hotmail.com>
2023-02-28 18:24:56 -0300

    estrutura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewPasswordComponent } from './pages/account/new-password/new-password.component';
+import { RecoveryPassComponent } from './pages/account/recovery-pass/recovery-pass.component';
+import { SingInComponent } from './pages/account/sing-in/sing-in.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewPasswordComponent,
+    RecoveryPassComponent,
+    SingInComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
