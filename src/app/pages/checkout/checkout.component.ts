1bf44057bcddd322bb5ee0f202b4d1bdc1f12723
Leticia <leticia.junqueira@doubleit.com.br>
2024-03-19 13:33:15 -0300

    ajustes

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { CartItem } from 'src/app/core/models/cart.model';
-import { CartService } from 'src/app/core/services/cart.service';
-import { ProductsService } from 'src/app/core/services/products.service';
 
 @Component({
   selector: 'app-checkout',
